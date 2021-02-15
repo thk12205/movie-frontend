403ebaff8e4c9183a215236eb9a6a3b1af0bf416
theo <thk12205@gmail.com>
2021-02-15 00:58:15 -0800

    add signup, login, logout pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 
 Vue.use(VueRouter);
 
@@ -16,8 +20,24 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: About
+    // component:  () =>
+    //   import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: "/logout",
+    name: "Logout",
+    component: Logout
   }
 ];
 
